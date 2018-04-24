1b0fcfab7f69d4374b9ef2495b4f374d09ab179d
Lucas Ricarte <lucasrrt@gmail.com>
2018-04-23 22:22:25 -0300

    user age

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	has_many :cards
+
+	def age
+		now = Time.now.to_date
+		return now.year - birthdate.year - ((now.month > birthdate.month || (now.month == birthdate.month and now.day >= birthdate.day))? 0:1)
+	end
 end
