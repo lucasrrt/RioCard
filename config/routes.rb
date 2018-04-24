3e8eafefd667c206b3d8ca0fb78a759045055c09
Lucas Ricarte <lucasrrt@gmail.com>
2018-04-23 21:22:35 -0300

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :travels
-  resources :vehicles
-  resources :recharges
-  resources :cards
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'welcome#index'
+
+	get 'welcome/index'
+
+	resources :travels
+	resources :vehicles
+	resources :recharges
+	resources :cards
+	resources :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
