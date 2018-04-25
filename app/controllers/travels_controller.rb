960fad78c43268bdcdc3130fd4178586ebb22373
Lucas Ricarte <lucasrrt@gmail.com>
2018-04-25 12:35:45 -0300

    transaction logic

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,74 +1,81 @@
 class TravelsController < ApplicationController
-  before_action :set_travel, only: [:show, :edit, :update, :destroy]
+	before_action :set_travel, only: [:show, :edit, :update, :destroy]
 
-  # GET /travels
-  # GET /travels.json
-  def index
-    @travels = Travel.all
-  end
+	# GET /travels
+	# GET /travels.json
+	def index
+		@travels = Travel.all
+	end
 
-  # GET /travels/1
-  # GET /travels/1.json
-  def show
-  end
+	# GET /travels/1
+	# GET /travels/1.json
+	def show
+	end
 
-  # GET /travels/new
-  def new
-    @travel = Travel.new
-  end
+	# GET /travels/new
+	def new
+		@travel = Travel.new
+	end
 
-  # GET /travels/1/edit
-  def edit
-  end
+	# GET /travels/1/edit
+	def edit
+	end
 
-  # POST /travels
-  # POST /travels.json
-  def create
-    @travel = Travel.new(travel_params)
+	# POST /travels
+	# POST /travels.json
+	def create
+		@travel = Travel.new(travel_params)
+		price = @travel.vehicle.price
+		balance = @travel.card.balance
 
-    respond_to do |format|
-      if @travel.save
-        format.html { redirect_to @travel, notice: 'Travel was successfully created.' }
-        format.json { render :show, status: :created, location: @travel }
-      else
-        format.html { render :new }
-        format.json { render json: @travel.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if balance > price
+				if @travel.save
+					format.html { redirect_to @travel, notice: 'Travel was successfully created.' }
+					format.json { render :show, status: :created, location: @travel }
+				else
+					format.html { render :new }
+					format.json { render json: @travel.errors, status: :unprocessable_entity }
+				end
+			else
+				format.html { render :insufficient_balance }
+				format.json { render json: {message: "insufficient balance"}, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /travels/1
-  # PATCH/PUT /travels/1.json
-  def update
-    respond_to do |format|
-      if @travel.update(travel_params)
-        format.html { redirect_to @travel, notice: 'Travel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @travel }
-      else
-        format.html { render :edit }
-        format.json { render json: @travel.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /travels/1
+	# PATCH/PUT /travels/1.json
+	def update
+		respond_to do |format|
+			if @travel.update(travel_params)
+				format.html { redirect_to @travel, notice: 'Travel was successfully updated.' }
+				format.json { render :show, status: :ok, location: @travel }
+			else
+				format.html { render :edit }
+				format.json { render json: @travel.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /travels/1
-  # DELETE /travels/1.json
-  def destroy
-    @travel.destroy
-    respond_to do |format|
-      format.html { redirect_to travels_url, notice: 'Travel was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+	# DELETE /travels/1
+	# DELETE /travels/1.json
+	def destroy
+		@travel.destroy
+		respond_to do |format|
+			format.html { redirect_to travels_url, notice: 'Travel was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_travel
-      @travel = Travel.find(params[:id])
-    end
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_travel
+		@travel = Travel.find(params[:id])
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def travel_params
-      params.require(:travel).permit(:vehicle_id, :card_id)
-    end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def travel_params
+		params.require(:travel).permit(:vehicle_id, :card_id)
+	end
 end
